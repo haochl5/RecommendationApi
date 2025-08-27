c40e4a22163e5bcee9f5645edf452a2f4fe8e3aa
Daniel Li <haochl5@uw.edu>
2025-08-26 20:02:41 -0700

    new changes to remove circular dependencies

--- src/main/java/helloworld/config/CustomAuthenticationProvider.java
+++ src/main/java/helloworld/config/CustomAuthenticationProvider.java
@@ -2,7 +2,7 @@ package helloworld.config;
 
 import helloworld.service.UserService;
 
-import org.hibernate.validator.internal.util.stereotypes.Lazy;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
