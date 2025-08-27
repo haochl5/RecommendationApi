a047b1fcc101e6063e4d081e43212e8354caf906
Daniel Li <haochl5@uw.edu>
2025-08-26 19:39:17 -0700

    attempt to use constructor injection

--- src/main/java/helloworld/service/UserServiceImpl.java
+++ src/main/java/helloworld/service/UserServiceImpl.java
@@ -5,7 +5,6 @@ import helloworld.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import jakarta.annotation.PostConstruct;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -13,13 +12,12 @@ import java.util.Optional;
 @Service
 public class UserServiceImpl implements UserService {
     
-    @Autowired
     private UserRepository userRepository;
-    
-    @Autowired
     private PasswordEncoder passwordEncoder;
     
-    public UserServiceImpl() {
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
     
     @Override
